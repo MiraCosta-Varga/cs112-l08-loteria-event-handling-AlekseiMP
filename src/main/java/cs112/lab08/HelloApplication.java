fdab63a4594be73ad5d96a545a85a8ea1cc70c4b
AlekseiMP <aleksei.merittpowell@gmail.com>
2024-11-03 11:35:57 -0800

    finished project

--- src/main/java/cs112/lab08/HelloApplication.java
+++ src/main/java/cs112/lab08/HelloApplication.java
@@ -1,13 +1,21 @@
 package cs112.lab08;
 
 import javafx.application.Application;
+import javafx.event.ActionEvent;
+import javafx.event.EventHandler;
 import javafx.fxml.FXMLLoader;
+import javafx.geometry.Pos;
 import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+import javafx.scene.layout.VBox;
+import javafx.scene.text.Font;
 import javafx.stage.Stage;
+import javafx.scene.image.ImageView;
 
 import java.io.IOException;
 
-public class HelloApplication extends Application {
+public class HelloApplication extends Application implements EventHandler<ActionEvent> {
 
     //CONSTANTS
 
@@ -19,10 +27,59 @@ public class HelloApplication extends Application {
             new LoteriaCard("La ingeniería", "9.png", 9),
     };
 
+    ImageView cardImageView;
+    Label messageLabel;
 
     @Override
     public void start(Stage stage) throws IOException {
-        //removed FXML code, fill this in with components, scene, stage, etc.
+        LoteriaCard cardLogo = new LoteriaCard();
+        //SETUP COMPONENTS
+        Label titleLabel = new Label("Welcome to EChALE STEM Loteria!");
+        ImageView cardImageView = new ImageView( cardLogo.getImage() );
+        Label messageLabel = new Label("Click the button below to randomly draw a card");
+        Button drawCardButton = new Button("Draw Random Card");
+        //CUSTOMIZE COMPONENTS
+        cardImageView.setFitWidth(300);
+        cardImageView.setPreserveRatio(true);
+        titleLabel.setFont(new Font(20.0));
+        drawCardButton.setOnAction(
+                new EventHandler<ActionEvent>() {
+                    @Override
+                    public void handle(ActionEvent actionEvent) {
+                            System.out.println("Button was clicked!");
+                            //set random card
+                            int randomInt = (int) (Math.random() * LOTERIA_CARDS.length); //gen 0-3
+                            LoteriaCard randomCard = LOTERIA_CARDS[randomInt];
+                            //image changes
+                            cardImageView.setImage(randomCard.getImage());
+                            //message label
+                            messageLabel.setText(randomCard.getCardName());
+                    }
+                }
+        );
+        //ADD COMPONENTS
+        VBox vbox = new VBox();
+        vbox.getChildren().addAll(titleLabel, cardImageView, messageLabel, drawCardButton);
+        vbox.setAlignment(Pos.CENTER);
+        vbox.setSpacing(5.0);
+        //SETUP SCENE AND SHOW
+        Scene scene = new Scene(vbox, 350, 500);
+        stage.setScene(scene);
+        stage.setTitle("EChALE STEM Loteria");
+        stage.show();
+    }
+
+    //HANDLE METHOD
+    @Override
+    public void handle(ActionEvent actionEvents) {
+        System.out.println("Button was clicked!");
+        //set random card
+        int randomInt = (int) (Math.random() * LOTERIA_CARDS.length); //gen 0-3
+        LoteriaCard randomCard = LOTERIA_CARDS[randomInt];
+        //image changes
+        cardImageView.setImage(randomCard.getImage());
+        //message label
+        messageLabel.setText(randomCard.getCardName());
     }
 
     public static void main(String[] args) {
